b5a3c20b45993c49b7a208dafb5cf6a16181d9b3
Ryan Williams <ryandub9@gmail.com>
2021-09-12 19:31:28 -0600

    adding static data for projects and experience

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,41 @@
 import 'tailwindcss/tailwind.css'
 import Link from 'next/link'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<div>
-			<nav>
-				<ul>
-					<li>
-						<Link href="/">(Home Icon)</Link>
-					</li>
-					<li>
-						<Link href="/portfolio">Portfolio</Link>
+			<header>
+				<nav className="mt-4 mb-10 mx-3">
+					<ul className="flex flex-row space-x-6">
+						<li className="font-bold mr-5 hover:text-gray-900">
+							<Link className="" href="/">RyanDev.io</Link>
 						</li>
-					<li>
-						<Link href="/resume">Resume</Link>
-					</li>
-					<li>
-						<Link href="/contact">Contact</Link>
-					</li>
-				</ul>
-			</nav>
-			<Component {...pageProps} />
+						<li className="hover:text-gray-900">
+							<Link href="/portfolio">Portfolio</Link>
+							</li>
+						<li className="hover:text-gray-900">
+							<Link href="/resume">Resume</Link>
+						</li>
+						<li className="hover:text-gray-900">
+							<Link href="/contact">Contact</Link>
+						</li>
+						<li className="hover:text-gray-900">
+							<a href="https://github.com/niterich" className="text-3xl">
+								<AiFillGithub></AiFillGithub>
+							</a>
+						</li>
+						<li className="hover:text-gray-900">
+							<a href="https://linkedin.com/in/ryanwilliams99" className="text-3xl">
+								<AiFillLinkedin></AiFillLinkedin>
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<div className="mx-auto mt- w-10/12">
+				<Component {...pageProps} />
+			</div>
 		</div>
 
 	)
